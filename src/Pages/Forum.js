b70cd92990c01ae42729bc472b44254691add860
Mike Bartels <mikebartels201@outlook.com>
2022-06-07 20:55:34 +0200

    test

--- src/Pages/Forum.js
+++ src/Pages/Forum.js
@@ -1,7 +1,11 @@
 import React from "react";
 
 function Forum() {
-  return <div>This is a Forum</div>;
+  return (
+    <>
+      <div>This is a Forum</div>;<div>Hello</div>;
+    </>
+  );
 }
 
 export default Forum;
