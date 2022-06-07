91c03bea3b10adf41f72bc1c3b59072089449cf9
Mike Bartels <mikebartels201@outlook.com>
2022-06-07 19:36:24 +0200

    Creating folder structures

--- src/components/Homepage/Homepage.js
+++ src/components/Homepage/Homepage.js
@@ -1,5 +1,4 @@
 import React from "react";
-import Header from "./header/Navbar";
 import LatestNews from "./LatestNews/LatestNews";
 import Hotnews from "./Hotnews/Hotnews";
 import "./Homepage.css";
@@ -7,7 +6,6 @@ import "./Homepage.css";
 function Homepage() {
   return (
     <div>
-      <Header />
       <div className="news-container">
         <div className="latest-news-container">
           <LatestNews />
