ee497d7173f46a8df60bfff5f3d209fc7fc2da24
Mike Bartels <mikebartels201@outlook.com>
2022-06-08 20:47:02 +0200

    design changes

--- src/components/Homepage/Hotnews/Hotnews.js
+++ src/components/Homepage/Hotnews/Hotnews.js
@@ -17,11 +17,17 @@ function Hotnews() {
             src="https://media.gettyimages.com/photos/georgios-giakoumakis-of-vvv-venlo-celebrates-during-the-dutch-match-picture-id1230569803?k=20&m=1230569803&s=612x612&w=0&h=o0axDpxdehvf_wFt-5flB8oApAxMuaCzu7d_Q89XDRI="
           />
           <Card.Body>
-            <Card.Title className="TitleClass">Card title</Card.Title>
+            <Card.Title className="CardTitle">
+              VVV trekt nieuwe speler aan!
+            </Card.Title>
             <Card.Text className="CardClass">
-              This is a wider card with supporting text below as a natural
-              lead-in to additional content. This content is a little bit
-              longer.
+              Lorem ipsum dolor, sit amet consectetur adipisicing elit. Pariatur
+              eveniet facere dicta aspernatur aliquam id quas, corporis est! A
+              qui, praesentium ipsam vitae perspiciatis impedit similique ab?
+              Harum, impedit sed. Lorem ipsum dolor sit amet, consectetur
+              adipisicing elit. Voluptatem eaque voluptatibus esse atque nobis
+              quisquam hic molestiae architecto, quam, soluta blanditiis ea vel
+              minus sint vitae inventore et? Unde, praesentium?
             </Card.Text>
           </Card.Body>
           <Card.Footer>
@@ -34,7 +40,7 @@ function Hotnews() {
             src="https://media.gettyimages.com/photos/thorsten-kirschbaum-of-vvv-venlo-georgios-giakoumakis-of-vvv-venlo-picture-id1232865974?k=20&m=1232865974&s=612x612&w=0&h=ULBNkjKsoUE4H0EBl3GLSkdSqsExVDbfeWCxLpQ5uD8="
           />
           <Card.Body>
-            <Card.Title>Card title</Card.Title>
+            <Card.Title className="CardTitle">Venlonaren Bedankt!</Card.Title>
             <Card.Text className="CardClass">
               This card has supporting text below as a natural lead-in to
               additional contentThis card has supporting text below as a natural
@@ -53,7 +59,7 @@ function Hotnews() {
             src="https://media.gettyimages.com/photos/supporters-of-vvv-venlo-during-the-dutch-eredivisie-match-between-de-picture-id1138336147?k=20&m=1138336147&s=612x612&w=0&h=o6g2AXJl2IyYj7eWEy_Q_wtL5nMFRnyUknonuXzZQhc="
           />
           <Card.Body>
-            <Card.Title>Card title</Card.Title>
+            <Card.Title className="CardTitle">Seizoens afsluiter</Card.Title>
             <Card.Text className="CardClass">
               This is a wider card with supporting text below as a natural
               lead-in to additional content. This card has even longer content
--- src/components/Homepage/LatestNews/LatestNews.js
+++ src/components/Homepage/LatestNews/LatestNews.js
@@ -10,14 +10,14 @@ function LatestNews() {
     <div>
       <h1>Latest News</h1>
       <CardGroup>
-        <Card border="danger">
+        <Card>
           <Card.Img
             variant="top"
             src="https://media.gettyimages.com/photos/georgios-giakoumakis-of-vvv-venlo-celebrates-during-the-dutch-match-picture-id1230569803?k=20&m=1230569803&s=612x612&w=0&h=o0axDpxdehvf_wFt-5flB8oApAxMuaCzu7d_Q89XDRI="
           />
           <Card.Body>
-            <Card.Title className="TitleClass">
-              VVV TREKT NIEUWE SPELER AAN!
+            <Card.Title className="CardTitle">
+              VVV trekt nieuwe speler aan!
             </Card.Title>
             <Card.Text className="CardClass">
               This is a wider card with supporting text below as a natural
@@ -29,13 +29,15 @@ function LatestNews() {
             <small className="text-muted">Last updated 3 mins ago</small>
           </Card.Footer>
         </Card>
-        <Card border="danger">
+        <Card>
           <Card.Img
             variant="top"
             src="https://media.gettyimages.com/photos/thorsten-kirschbaum-of-vvv-venlo-georgios-giakoumakis-of-vvv-venlo-picture-id1232865974?k=20&m=1232865974&s=612x612&w=0&h=ULBNkjKsoUE4H0EBl3GLSkdSqsExVDbfeWCxLpQ5uD8="
           />
           <Card.Body>
-            <Card.Title>Card title</Card.Title>
+            <Card.Title className="CardTitle">
+              Rick Kruijs nieuwe trainer VVV
+            </Card.Title>
             <Card.Text className="CardClass">
               This card has supporting text below as a natural lead-in to
               additional contentThis card has supporting text below as a natural
@@ -48,13 +50,15 @@ function LatestNews() {
             <small className="text-muted">Last updated 3 mins ago</small>
           </Card.Footer>
         </Card>
-        <Card border="danger">
+        <Card>
           <Card.Img
             variant="top"
             src="https://media.gettyimages.com/photos/supporters-of-vvv-venlo-during-the-dutch-eredivisie-match-between-de-picture-id1138336147?k=20&m=1138336147&s=612x612&w=0&h=o6g2AXJl2IyYj7eWEy_Q_wtL5nMFRnyUknonuXzZQhc="
           />
           <Card.Body>
-            <Card.Title>Card title</Card.Title>
+            <Card.Title className="CardTitle">
+              10de plaats eindklassering
+            </Card.Title>
             <Card.Text className="CardClass">
               This is a wider card with supporting text below as a natural
               lead-in to additional content. This card has even longer content
